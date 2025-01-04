123b061149ecd46e13d241bb2743fc89636037ac
dibyajyoti79 <dibyajyoti274@gmail.com>
2025-01-04 10:43:53 +0530

    feat: Add a req res logger

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -1,6 +1,7 @@
 import { LogEntry, LogLevel } from '../interfaces/LogEntry'
 import CloudWatchLogger from './CloudWatchLogger'
-
+import { Request, Response, NextFunction } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 class Logger {
     private cloudWatchLogger: CloudWatchLogger
     private service: string
@@ -44,6 +45,48 @@ class Logger {
 
     trace(entry: LogEntry): Promise<void> {
         return this.log(entry, LogLevel.TRACE)
+    }
+
+    /**
+     * Middleware for logging HTTP requests and responses.
+     */
+    logHttp(): (req: Request, res: Response, next: NextFunction) => void {
+        return (req: Request, res: Response, next: NextFunction) => {
+            const start = Date.now()
+
+            res.on('finish', async () => {
+                const duration = Date.now() - start
+                const correlationId = Array.isArray(req.headers['x-correlation-id'])
+                    ? req.headers['x-correlation-id'][0]
+                    : req.headers['x-correlation-id'] || uuidv4()
+                const sanitizedHeaders = { ...req.headers }
+                delete sanitizedHeaders['authorization'] // Remove sensitive information
+                const logEntry: LogEntry = {
+                    message: 'HTTP Transaction',
+                    traceId: correlationId,
+                    body: {
+                        statusCode: res.statusCode,
+                        method: req.method,
+                        path: req.path,
+                        url: req.originalUrl || req.url,
+                        query: req.query,
+                        headers: sanitizedHeaders,
+                        userAgent: req.headers['user-agent'] || '',
+                        ip: req.ip || '',
+                        duration: `${duration}ms`
+                    },
+                    timestamp: new Date().toISOString()
+                }
+
+                try {
+                    await this.info(logEntry)
+                } catch (error) {
+                    console.error('Failed to log HTTP transaction:', error)
+                }
+            })
+
+            next()
+        }
     }
 }
 
